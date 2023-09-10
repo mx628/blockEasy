c224f7a434c23d88a3d0eb9787bba09e56ffbb53
mx628 <143996710+mx628@users.noreply.github.com>
2023-09-10 20:04:34 +0300

    Add files via upload

---
+++ test-main/test/itc_17-18.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "easy.h"
+
+using namespace std;
+
+bool itc_ispositive(int num){
+    if(num >= 0)
+        return true;
+    return false;
+}
+
+bool itc_ispositive_d(double num){
+    if(num >= 0)
+        return true;
+    return false;
+}
---
+++ test-main/test/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "easy.h"
+
+using namespace std;
+
+int main()
+{
+    cout <<itc_pow(3, 3);
+    return 0;
+}
