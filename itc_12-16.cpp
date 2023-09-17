033efee57d49e7a24fe41cd413448775b93c9e8c
Boris Tryukhan <143996710+mx628@users.noreply.github.com>
2023-09-17 15:14:18 +0300

    itc_12-16.cpp

--- itc_12-16.cpp
+++ itc_12-16.cpp
@@ -16,6 +16,8 @@ int itc_spr(int a, int b){
 }
 
 int itc_str(int a, int b, int c){
+    if(a <= 0 || b <= 0 || c <= 0)
+        return -1;
     if(a + b <= c || a + c <= b || b + c <= a)
         return -1;
     int p = (a + b + c) / 2;
